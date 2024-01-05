8a5e17a23928c049d851637da052f65e6b1ef86f
Yehuda Fruchter <osseonews@osseonews.com>
2024-01-05 13:33:24 -0500

    with vike

--- _worker.js
+++ _worker.js
@@ -2,55 +2,25 @@ import {
   error,      // creates error responses
   json,       // creates JSON responses
   //html,       // creates HTML responses
-  StatusError,
+  //StatusError,
   Router,     // the ~440 byte router itself
   withParams, // middleware: puts params directly on the Request
 } from 'itty-router'
+import { renderPage } from 'vike/server'
 
-
-const todos = [
-  { id: '1', message: 'Pet the puppy' },
-  { id: '2', message: 'Pet the kitty' },
-]
-// then pass them to the Router
 const router = Router()
-
 
 router
   // add some middleware upstream on all routes
   .all('*', withParams)
-  /*EXAMPLES*/
-  // ise .get for GET list of todos with authorization
-  .get('/todos', (params) => {
-    //console.log ("params query", params.query?.id) //if send ?id=
-    const ID = params?.query?.id
-    if (ID) {
-      const todo = todos.find(t => t.id === ID)
-      return todo
-    }
-    //console.log ("env", env.BASIC_TOKEN)
-    return { todos }
-  })
-
-  // GET single todo, by ID -  of :/id
-  .get('/todos/:id',
-    ({ id }) => {
-      const todo = todos.find(t => t.id === id)
-      if (todo) {
-        return todo
-      }
-      throw new StatusError(400, 'No Todo.')
-    }
-  )
-
-
-  /*END OF EXAMPLES*/
-
-
-  // 
-  .all('*', (req, env) => {
+  // Pages requirement: Serve the static assets.
+  // Without this, the Worker will error and no assets will be served.
+  .all('/assets/*', (req, env) => {
     return env.ASSETS.fetch(req);
   })
+  //everything else serve with Vike 
+  .all('*', withVike)
+
 
 // Example: Cloudflare Worker module syntax
 export default {
@@ -59,4 +29,35 @@ export default {
       .handle(req, env, ctx)
       .then(json)     // send as JSON
       .catch(error),  // catch errors
+}
+
+// async Vike middleware
+const withVike = async (req, env) => {
+  const userAgent = req.headers.get('User-Agent') || ""
+  console.log('ua', userAgent)
+  const response = await handleSsr(req.url, userAgent, env)
+  if (response !== null) return response
+}
+
+async function handleSsr(url, userAgent, env) {
+  //get KV Data - you need try catch here and return a backup array in case KV failes, which it can sometimes - I've seen outages
+  const value = await env.POST_STORE.list();
+  //"list_complete": true,
+  //"cacheStatus": null
+  //"keys" has the data
+  const postsData = value.keys
+  const pageContextInit = {
+    urlOriginal: url,
+    userAgent,
+    postsData: postsData
+  }
+  const pageContext = await renderPage(pageContextInit)
+  const { httpResponse } = pageContext
+  if (!httpResponse) {
+    return null
+  } else {
+    const { statusCode: status, headers } = httpResponse
+    const stream = httpResponse.getReadableWebStream()
+    return new Response(stream, { headers, status })
+  }
 }
